d8a1518b09586f6d35e1c432d6b7ab692e36a41c
Patrick Toninato <pmtoninato@gmail.com>
2021-08-13 22:21:52 -0500

    add transaction querries

--- src/controllers/Import/ImportController.ts
+++ src/controllers/Import/ImportController.ts
@@ -1,7 +1,10 @@
 import { Request, Response } from 'express';
+import LeagueKeyHelper from '../../Helpers/LeagueKeyHelper';
 import leagueDao from '../../services/DataAccess/leagueDao';
+import SeasonDao from '../../services/DataAccess/SeasonDao';
 import seasonDao from '../../services/DataAccess/SeasonDao';
 import seasonImporter from '../../services/Importers/seasonImporter';
+import transactionApiSerivce from '../../services/api/YahooApi/transactionApiService';
 
 export async function ImportLeague(
   req: Request,
@@ -26,18 +29,45 @@ export async function ImportSeason(
   console.log('here');
 
   const league_key = req.query.league_key.toString();
-  const leagueKeySplit = league_key.split('.');
-  const yahooGameKey = leagueKeySplit[0];
-  const yahooleagueId = leagueKeySplit[2];
+
+  const leagueKeyParam = await LeagueKeyHelper.SplitLeagueKey(league_key);
 
   let season = await seasonDao.GetSeasonByYahooLeagueId(
-    <number>(<unknown>yahooleagueId)
+    <number>(<unknown>leagueKeyParam.league_id)
   );
 
   if (season == null) {
     season = await seasonImporter.importSeason(league_key);
   }
 
+  console.log(season);
+
+  return res.json();
+}
+
+export async function ImportTransactions(
+  req: Request,
+  res: Response
+): Promise<Response> {
+  console.log('here');
+
+  const league_key = req.query.league_key.toString();
+
+  const leagueKeyParam = await LeagueKeyHelper.SplitLeagueKey(league_key);
+
+  const league = await leagueDao.GetOrImportLeague(leagueKeyParam.league_key);
+
+  const season = await SeasonDao.GetOrImportSeason(leagueKeyParam);
+
+  const transactions = await transactionApiSerivce.GetTransactionsByLeagueKey(
+    leagueKeyParam.league_key
+  );
+
+  const transaaction = transactions.transactions[0];
+
+  console.log(transaaction.players);
+
+  console.log(league);
   console.log(season);
 
   return res.json();
