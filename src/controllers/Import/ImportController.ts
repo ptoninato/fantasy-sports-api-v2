7b969d4c4cdac9b9d3a8acc94a86112fca38e0e1
Patrick Toninato <pmtoninato@gmail.com>
2021-09-20 07:44:13 -0500

    refactor

--- src/controllers/Import/ImportController.ts
+++ src/controllers/Import/ImportController.ts
@@ -47,6 +47,7 @@ import seasonPositionDao from '../../services/DataAccess/seasonPositionDao';
 import matchupRosterDao from '../../services/DataAccess/matchupRosterDao';
 import matchupTeamDao from '../../services/DataAccess/matchupTeamDao';
 import { sleep } from '../../Helpers/Utility/Sleep';
+import MatchupRosterImporter from '../../services/Importers/MatchupRosterImporter';
 
 export async function ImportLeague(
   req: Request,
@@ -186,75 +187,9 @@ export async function ImportDailyMatchupRosterPlayerStatsForYear(
   res: Response
 ): Promise<Response> {
   const league_key = req.query.league_key.toString();
-  const year = <string>(<unknown>req.query.year);
-
   const leagueKeyParam = await LeagueKeyHelper.SplitLeagueKey(league_key);
 
-  const results = await MatchupTeamDatesDao.GetMlbRecordsBySeasonYear(
-    leagueKeyParam.league_id
-  );
+  await MatchupRosterImporter.ImportRosterAllDays(leagueKeyParam);
 
-  const season = await seasonDao.GetSeasonByYahooLeagueId(
-    <number>(<unknown>leagueKeyParam.league_id)
-  );
-  const gameCodeType = await gameCodeTypeDao.getTypeForSeason(season.seasonid);
-
-  const league = await leagueDao.GetLeagueByLeagueName('The League');
-
-  let timeoutCount = 0;
-
-  for (let x = 0; x < results.length; x++) {
-    const matchupTeam = results[x];
-
-    const matchupStartDate = moment(matchupTeam.startdate);
-    const matchupEndDate = moment(matchupTeam.enddate);
-    const teamKey = `${matchupTeam.yahoogamekey}.l.${matchupTeam.yahooleagueid}.t.${matchupTeam.yahooteamid}`;
-
-    const matchupTeamModel = await matchupTeamDao.GetMatchupTeamByMatchupTeamId(
-      matchupTeam.matchupteamid
-    );
-
-    for (
-      const m = moment(matchupStartDate);
-      m.diff(matchupEndDate, 'days') <= 0;
-      m.add(1, 'days')
-    ) {
-      if (timeoutCount == 190) {
-        console.log('waiting');
-        await sleep(180000);
-        timeoutCount = 0;
-      }
-
-      const roster = await rosterApiService.GetRosterForDay(
-        matchupTeam.yahooteamid.toString(),
-        leagueKeyParam,
-        m.format('YYYY-MM-DD')
-      );
-
-      timeoutCount++;
-
-      for (let r = 0; r < roster.roster.length; r++) {
-        const rosterSpot = roster.roster[r];
-
-        const player = await PlayerDao.GetOrImportPlayerUsingGameCodeTypeAndRoster(
-          rosterSpot,
-          gameCodeType
-        );
-
-        const seasonPosition = await seasonPositionDao.GetSeasonPostion(
-          season.seasonid,
-          rosterSpot.selected_position
-        );
-
-        const rosterPostion = await matchupRosterDao.GetOrImportMatchupRoster(
-          rosterSpot,
-          m.format('YYYY-MM-DD'),
-          seasonPosition,
-          matchupTeamModel,
-          player
-        );
-      }
-    }
-  }
   return res.json();
 }
