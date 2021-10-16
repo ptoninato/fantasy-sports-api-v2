7e3d847834b4ffa8963d240b99eb5a04e483da52
Patrick Toninato <pmtoninato@gmail.com>
2021-10-15 20:57:18 -0500

    player stats for week

--- src/services/api/YahooApi/playerApiService.ts
+++ src/services/api/YahooApi/playerApiService.ts
@@ -5,7 +5,11 @@ async function GetPlayerStatsForWeek(
   playerKey: string,
   week: number
 ): Promise<PlayerStats> {
-  const returnedData = await YahooFantasy.yf.player.stats(playerKey, week);
+  const weekString = <string>(<unknown>week);
+  const returnedData = await YahooFantasy.yf.player.stats(
+    playerKey,
+    weekString
+  );
   const playerStats = <PlayerStats>returnedData;
   return playerStats;
 }
